bfa2e862c1ad0a76ff80ab88a94483b94f87f793
manasvi6769-bit <manasvi6769@gmail.com>
2025-12-02 12:44:30 +0530

    localrepo_factorial

---
+++ a.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int n, p=1;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+       p*=i;
+       cout<<"factorial of "<<i<<":"<<p<<endl;
+    }
+
+    }
